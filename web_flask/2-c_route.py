4c8c8a9830a6b9b47f224e82cf1ce50ba74b31c8
Izy4eze <chibuezeokorieone@gmail.com>
2023-04-22 07:13:02 +0000

    Task done

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+"""starts Flask web app
+Routes:
+      / - display "Hello HBNB!"
+      /hbnb - display "HBNB"
+      /c/<text> - display "C <text>"
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb_route():
+    """prints Hello HBNB"""
+    return "Hello HBNB!"
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    """prints HBNB"""
+    return "HBNB"
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text):
+    """prints C followed by <text> content"""
+    text = text.replace("_", " ")
+    return "C %s" % text
+
+if --name__ == "__main__":
+     app.run(host="0.0.0.0")
