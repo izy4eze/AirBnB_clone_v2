8946eff204f33153cb92686a95109d16d86dde05
Izy4eze <chibuezeokorieone@gmail.com>
2023-04-22 07:21:41 +0000

    corrected

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python
-"""starts Flask web app
+#!/usr/bin/python3
+"""Starts Flask web app
 Routes:
-      / - display "Hello HBNB!"
-      /hbnb - display "HBNB"
-      /c/<text> - display "C <text>"
+    / - display "Hello HBNB!"
+    /hbnb - display "HBNB"
+    /c/<text> - display "C <text>"
 """
 from flask import Flask
 
@@ -15,10 +15,12 @@ def hbnb_route():
     """prints Hello HBNB"""
     return "Hello HBNB!"
 
-@app.route('/', strict_slashes=False)
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """prints HBNB"""
     return "HBNB"
+
 
 @app.route('/c/<string:text>', strict_slashes=False)
 def c_text(text):
@@ -26,5 +28,6 @@ def c_text(text):
     text = text.replace("_", " ")
     return "C %s" % text
 
+
 if __name__ == "__main__":
-     app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0")
