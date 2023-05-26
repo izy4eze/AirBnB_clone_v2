f42f6b97ab8b82d8086f2a12030c097114d27fd8
izy4eze <chibuezeokorieone@gmail.com>
2023-05-26 18:55:53 +0000

    done

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """Starts Flask web app
 Listening on 0.0.0.0:5000
-Route 'airbnb_onepage' displays "Hello HBNB!"
+Route 'airbnb-onepage' displays "Hello HBNB!"
 """
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('airbnb_onepage', strict_slashes=False)
+@app.route('airbnb-onepage', strict_slashes=False)
 def hello_route():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0:5000")
